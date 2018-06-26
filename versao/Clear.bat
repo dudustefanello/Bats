70b0b2da5cf5437537af6ebb0e5eb3ae418dbacc
Eduardo Stefanello <eduardo@stefanello.cc>
2018-06-26 09:20:09 -0300

    Não limpar ACBr

--- versao/Clear.bat
+++ versao/Clear.bat
@@ -6,7 +6,5 @@
   @ del /s C:\Fontes\flextotal_5_0_%2\Integracao\*.dcu;
 @ %LOGGING% Limpar NucleoFlextotal
   @ del /s C:\Fontes\flextotal_5_0_%2\NucleoFlextotal\*.dcu;
-@ %LOGGING% Limpar ACBr
-  @ del /s C:\Fontes\compTerceiros\xe10.2\acbr2\*.dcu;
 
 @ exit
