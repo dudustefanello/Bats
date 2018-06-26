2c3d9d7ba20257c83ac2a41611f6efbf98847de2
Eduardo Stefanello <eduardo@stefanello.cc>
2018-06-26 09:29:45 -0300

    Path esquecido

--- versao/Commit.bat
+++ versao/Commit.bat
@@ -2,14 +2,14 @@
 @ echo %linha:~3,14%^|^ %linha:~19,16%^|^ %linha:~37,10% > C:\Users\eduar\OneDrive\Documentos\Bats\versao\toCommit%2.txt
 
 @ %LOGGING% Commit C:\Fontes\flextotal_5_0_%2
-  @ TortoiseProc.exe /path:"C:\Fontes\flextotal_5_0_%2" /closeonend:3 /command:commit /logmsgfile:"C:\Users\eduar\OneDrive\Documentos\Bats\versao\toCommit%2.txt"
+  @ TortoiseProc.exe /path:"C:\Fontes\flextotal_5_0_%2" /closeonend:3 /command:commit /logmsgfile:"C:\Users\eduar\GitHub\Bats\versao\toCommit%2.txt"
 
 @ %LOGGING% Commit C:\Distribuicao\%REPOSITORIO%
-  @ TortoiseProc.exe /path:"C:\Distribuicao\%REPOSITORIO%" /closeonend:3 /command:commit /logmsgfile:"C:\Users\eduar\OneDrive\Documentos\Bats\versao\toCommit%2.txt"
+  @ TortoiseProc.exe /path:"C:\Distribuicao\%REPOSITORIO%" /closeonend:3 /command:commit /logmsgfile:"C:\Users\eduar\GitHub\Bats\versao\toCommit%2.txt"
 
 @ if %2 equ %TRUNK% (
     @ %LOGGING% Commit C:\Fontes\trunk\Change.log
-      @ TortoiseProc.exe /path:"C:\Fontes\trunk\FlexTotal\Change.log" /closeonend:3 /command:commit /logmsgfile:"C:\Users\eduar\OneDrive\Documentos\Bats\versao\toCommit%2.txt"
+      @ TortoiseProc.exe /path:"C:\Fontes\trunk\FlexTotal\Change.log" /closeonend:3 /command:commit /logmsgfile:"C:\Users\eduar\GitHub\Bats\versao\versao\toCommit%2.txt"
   )
 
 @ exit
