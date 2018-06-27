19385550074cd3f43214df3436712f10b4d05fa7
Eduardo Stefanello <eduardo@stefanello.cc>
2018-06-27 11:09:47 -0300

    Finalizar Chamado
    Scripts para salvar um executável

---
+++ chamados/info.bat
@@ -0,0 +1,5 @@
+XCopy /q C:\Users\eduar\OneDrive\Documentos\Notas\info.md C:\Users\eduar\GitHub\Bats\chamados /y
+
+C:\Users\eduar\GitHub\Bats\chamados\info.py
+
+del /q C:\Users\eduar\GitHub\Bats\chamados\info.md
---
+++ chamados/info.py
@@ -0,0 +1,57 @@
+import pyperclip
+import subprocess
+import time
+
+chamado = str()
+texto = str()
+partes = tuple()
+linhas = list(str())
+
+link = 'http://104.236.197.223/sac/issues/'
+
+titulos = [('###### ','h6. '),('##### ','h5. '),('#### ','h4. '),('### ','h3. '),('## ','h2. '),('# ','h1. ')]
+
+indices = [('      - ', '      * '),('    - ', '    * '),('  - ', '  * '),('- ', '* '),
+           ('      1. ','      # '),('    1. ','    # '),('  1. ','  # '),('1. ','# ')]
+
+delphi  = [(' ``', ' <code class="Delphi">'),('`` ', '</code> '),('``.', '</code>.'),
+           ('``,', '</code>,'),('``:', '</code>:'),('``;', '</code>;')]
+
+codigos = [(' `', ' @'),('` ', '@ '),('`,', '@,'),('`.', '@.'),('`:', '@:'),('`;', '@;')]
+
+negrito = [(' **', ' *'),('** ', '* '),('**,', '*,'),('**.', '*.'),('**:', '*:'),('**;', '*;')]
+
+arquivo = open('info.md', 'r')
+texto = arquivo.read()
+
+partes = texto.rpartition('#info')
+chamado = partes[0]
+linhas = partes[2].splitlines(True);
+
+texto = ''
+
+for linha in linhas:
+    for i in titulos:
+        if linha.rfind(i[0]) >= 0:
+            linha = linha.replace(i[0], i[1])
+            linha = linha.replace('\n', '\n\n')
+
+    for i in delphi:
+        linha = linha.replace(i[0], i[1])
+
+    for i in codigos:
+        linha = linha.replace(i[0], i[1])
+
+    for i in negrito:
+        linha = linha.replace(i[0], i[1])
+
+    for i in indices:
+        linha = linha.replace(i[0], i[1])
+
+    texto += linha
+
+chamado = chamado.replace('#', '')
+subprocess.call(['C:/Users/eduar/AppData/Local/Vivaldi/Application/vivaldi', '--new-window', link + chamado])
+pyperclip.copy('h1. #info\n\n' + texto);
+
+subprocess.call(['TortoiseProc.exe', '/path:C:/Fontes/trunk', '/closeonend:3', '/command:commit', '/logmsg:' + '#' + chamado])
