0bda706fe3d3afdaab4f748230aa0df47a11138c
Eduardo Stefanello <eduardo@stefanello.cc>
2018-07-10 14:12:01 -0300

    Compactar pelo parâmetro

--- versao/Compactar.bat
+++ versao/Compactar.bat
@@ -1,11 +1,11 @@
 @ if %2 neq %TRUNK% (
   @ %LOGGING% Compactar Executaveis
     @ %LOGGING% Compactar FlexSmart
-      @ start C:\Users\eduar\GitHub\Bats\versao\upx.exe C:\Fontes\flextotal_5_0_%TESTE%\Flextotal\bin\Retaguarda\FlexTotal.exe
+      @ start C:\Users\eduar\GitHub\Bats\versao\upx.exe C:\Fontes\flextotal_5_0_%2\Flextotal\bin\Retaguarda\FlexTotal.exe
     @ %LOGGING% Compactar Importador
-      @ start C:\Users\eduar\GitHub\Bats\versao\upx.exe C:\Fontes\flextotal_5_0_%TESTE%\Flextotal\bin\Retaguarda\ImportacaoPedidosVenda.exe
+      @ start C:\Users\eduar\GitHub\Bats\versao\upx.exe C:\Fontes\flextotal_5_0_%2\Flextotal\bin\Retaguarda\ImportacaoPedidosVenda.exe
     @ %LOGGING% Compactar Integrador
-      @ start /wait C:\Users\eduar\GitHub\Bats\versao\upx.exe C:\Fontes\flextotal_5_0_%TESTE%\Flextotal\bin\Retaguarda\IntegracaoPAFECF.exe
+      @ start /wait C:\Users\eduar\GitHub\Bats\versao\upx.exe C:\Fontes\flextotal_5_0_%2\Flextotal\bin\Retaguarda\IntegracaoPAFECF.exe
     @ waitfor /t 2 Compactar
   )
 @ exit
